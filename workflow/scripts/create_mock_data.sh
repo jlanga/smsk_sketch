428890b28c0dcae2b4ddf375fe35aae375ab4703
Jorge Langa <6546145+jlanga@users.noreply.github.com>
2024-05-22 15:16:57 +0200

    fix(test): put correct chromosome name

--- workflow/scripts/create_mock_data.sh
+++ workflow/scripts/create_mock_data.sh
@@ -11,32 +11,32 @@ pushd resources/genomes
 
 wget \
     --continue \
-    https://ftp.ensembl.org/pub/release-112/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.22.fa.gz \
-    https://ftp.ensembl.org/pub/release-112/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.chromosome.19.fa.gz \
-    https://ftp.ensembl.org/pub/release-112/fasta/danio_rerio/dna/Danio_rerio.GRCz11.dna.chromosome.25.fa.gz
+    https://ftp.ensembl.org/pub/release-112/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.MT.fa.gz \
+    https://ftp.ensembl.org/pub/release-112/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.chromosome.MT.fa.gz \
+    https://ftp.ensembl.org/pub/release-112/fasta/danio_rerio/dna/Danio_rerio.GRCz11.dna.chromosome.MT.fa.gz
 
 popd
 
 wgsim \
-    -N 10000 \
+    -N 1000 \
     -1 150 \
     -2 150 \
-    resources/genomes/Homo_sapiens.GRCh38.dna.chromosome.22.fa.gz \
+    resources/genomes/Homo_sapiens.GRCh38.dna.chromosome.MT.fa.gz \
     >(seqtk seq -F I | gzip -9 > resources/reads/human_1.fq.gz) \
     >(seqtk seq -F I | gzip -9 > resources/reads/human_2.fq.gz)
 
 wgsim \
-    -N 10000 \
+    -N 1000 \
     -1 150 \
     -2 150 \
-    resources/genomes/Mus_musculus.GRCm39.dna.chromosome.19.fa.gz \
+    resources/genomes/Mus_musculus.GRCm39.dna.chromosome.MT.fa.gz \
     >(seqtk seq -F I | gzip -9 > resources/reads/mouse_1.fq.gz) \
     >(seqtk seq -F I | gzip -9 > resources/reads/mouse_2.fq.gz)
 
 wgsim \
-    -N 10000 \
+    -N 1000 \
     -1 150 \
     -2 150 \
-    resources/genomes/Danio_rerio.GRCz11.dna.chromosome.25.fa.gz \
+    resources/genomes/Danio_rerio.GRCz11.dna.chromosome.MT.fa.gz \
     >(seqtk seq -F I | gzip -9 > resources/reads/zebrafish_1.fq.gz) \
     >(seqtk seq -F I | gzip -9 > resources/reads/zebrafish_2.fq.gz)
